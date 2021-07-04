23598028c42c0078d076fa5315b47b074432ae1b
Daniel_Ruiz <daniel.ruiz@chironix.com>
2021-07-04 19:39:44 +0800

    removed .txt from  file names

--- common_procedures.sh
+++ common_procedures.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 #TODO: integrate this tool with fuzzy finders
-#TODO: Add alias to make adding a new procedure easy
-PTH_TXT_FLS=~/common_procedures/text_files
+#TODO: Add function to make adding a new procedure easy
+PTH_TXT_FLS=~/__common_procedures/text_files
 #TODO: programatically expand this based on the number of .txt files (use array and for loop)
 ARG1=git_commands
 ARG2=networking
@@ -11,7 +11,7 @@ ARG5=vscode
 
 
 echo " "
-cat $PTH_TXT_FLS/intro.txt
+cat $PTH_TXT_FLS/intro
 echo " "
 
 #TODO: programatically expand the possible arguments based on the number of .txt files
@@ -21,6 +21,7 @@ complete -W "$ARG1 $ARG2 $ARG3 $ARG4 $ARG5" common_procedures
 #function for command to show reminders for common procedures
 common_procedures() {
     echo " "
-    cat $PTH_TXT_FLS/$1.txt
+    cat $PTH_TXT_FLS/$1
     echo " "
 }
+ #display_available_procedures
