9a4b7b09def7ea8fd4daa501f4eb5b95a3df4d35
Daniel_Ruiz <daniel.ruiz@chironix.com>
2021-07-04 15:20:10 +0800

    Script no longer needs case statement. Added another procedure to git commands

--- common_procedures.sh
+++ common_procedures.sh
@@ -12,31 +12,34 @@ echo " "
 
 #TODO: programatically expand the possible arguments based on the number of .txt files
 #set tab complete arguments
-complete -W "--$ARG1 --$ARG2 --$ARG3" common_procedures
+complete -W "$ARG1 $ARG2 $ARG3 $ARG4" common_procedures
 
 #function for command to show reminders for common procedures
 #TODO: Make this a for loop. Or integrate this in to fzf. Whichever is easier.
 common_procedures() {
-    case $@ in
-        --$ARG1)
-          echo " "
-          cat $PTH_TXT_FLS/$ARG1.txt
-          echo " "
-          ;;
-        --$ARG2)
-          echo " "
-          cat $PTH_TXT_FLS/$ARG2.txt
-          echo " "
-          ;;
-        --$ARG3)
-          echo " "
-          cat $PTH_TXT_FLS/$ARG3.txt
-          echo " "
-          ;;
-        --$ARG4)
-          echo " "
-          cat $PTH_TXT_FLS/$ARG4.txt
-          echo " "
-          ;;
-    esac
+    # case $@ in
+    #     --$ARG1)
+    #       echo " "
+    #       cat $PTH_TXT_FLS/$ARG1.txt
+    #       echo " "
+    #       ;;
+    #     --$ARG2)
+    #       echo " "
+    #       cat $PTH_TXT_FLS/$ARG2.txt
+    #       echo " "
+    #       ;;
+    #     --$ARG3)
+    #       echo " "
+    #       cat $PTH_TXT_FLS/$ARG3.txt
+    #       echo " "
+    #       ;;
+    #     --$ARG4)
+    #       echo " "
+    #       cat $PTH_TXT_FLS/$ARG4.txt
+    #       echo " "
+    #       ;;
+    # esac
+    echo " "
+    cat $PTH_TXT_FLS/$1.txt
+    echo " "
 }
