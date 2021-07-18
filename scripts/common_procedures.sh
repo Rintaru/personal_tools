bcb3ec8243fa2b35496d559a75e51ca6f9ec08ec
Daniel_Ruiz <daniel.ruiz@chironix.com>
2021-07-18 22:45:02 +0800

    tab complete now programatically expands based on number of txt files. And no longer using python script

--- scripts/common_procedures.sh
+++ scripts/common_procedures.sh
@@ -1,31 +1,16 @@
-#!/bin/bash -e
+#!/bin/bash
 #TODO: integrate this tool with fuzzy finders
 #TODO: Add function to make adding a new procedure easy
 PTH_TXT_FLS=$HOME/__common_procedures/text_files
 PTH_SCRIPTS=$HOME/__common_procedures/scripts
-#TODO: programatically expand this based on the number of .txt files (use array and for loop)
-ARG1=git_commands
-ARG2=networking
-ARG3=ros
-ARG4=terminator
-ARG5=vscode
-ARG6=ros_service
-
-
-#Create tmp file for autocomplete function
-#tmpfile=$(mktemp /tmp/common_procedures.XXXXXXX)
-#exec 3>"$tmpfile"
-#exec 4<"$tmpfile"
 
 
 echo " "
 cat $PTH_TXT_FLS/intro
 echo " "
-#TODO: programatically expand the possible arguments based on the number of .txt files
-#TODO: use 'complete -F _common_procedures' to dynamically show possible arguments based on previous arguments
 #set tab complete arguments
 #complete -W "$ARG1 $ARG2 $ARG3 $ARG4 $ARG5 $ARG6 --create --add" common_procedures
-complete -F _common_procedures common_procedures
+complete -o nospace -C _common_procedures common_procedures
 
 #function for command to show reminders for common procedures
 common_procedures() {
@@ -49,11 +34,10 @@ common_procedures() {
 
 
 }
-
+#function to load tab complete arguments
 _common_procedures() {
 
-  # args=$(python $PTH_SCRIPTS/auto_complete.py -a $PTH_TXT_FLS -b $@)
-  
-  COMPREPLY=$(python $PTH_SCRIPTS/auto_complete.py $@)
-  
+    cd $PTH_TXT_FLS
+    printf "%s\n" "$2"*
+
 }
