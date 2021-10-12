90f3a333cc2d7e00aba639725122b452ea43d6b2
sekiro <shintaruruiz@gmail.com>
2021-10-12 12:04:52 +0800

    update install script

--- scripts/install_ros.sh
+++ scripts/install_ros.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl # if you haven't already installed curl
+sudo apt install -y curl # if you haven't already installed curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt install ros-melodic-ros-base
+sudo apt install -y ros-melodic-ros-base
 source /opt/ros/melodic/setup.bash
-sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo rosdep init
 rosdep update
