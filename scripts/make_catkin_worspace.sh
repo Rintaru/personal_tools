7909458dbb4e241279601b714ef6d402b01e72ac
Daniel_Ruiz <shintaruruiz@gmail.com>
2022-07-22 22:43:38 +0800

    create catkin workspace script

---
+++ scripts/make_catkin_worspace.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mk_catkin_ws(){  
+  if [ $# -eq 0 ]
+    then
+      echo "mk_catkin_ws needs an arg!"
+    else
+      if [ -e "$1" ]; then
+          echo "$1 exists, use a different dir name."
+      else
+          mkdir -v -p $1
+      fi
+  fi 
+
+}
