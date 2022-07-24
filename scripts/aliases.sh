c01a30cc0b55c52d959545138903253eaf1c7994
Daniel_Ruiz <shintaruruiz@gmail.com>
2022-07-24 14:02:57 +0800

    added commands

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,3 +1,4 @@
 alias edbash='nano /home/sekiro/.bashrc'
 alias edcomm='code /home/sekiro/.common_procedures/'
 alias edalias='nano /home/sekiro/.common_procedures/scripts/aliases.sh'
+alias john-repelant='gsettings set org.gnome.desktop.session idle-delay'
