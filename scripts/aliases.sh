d9818aa31faf1bb841d69df146b8186c1c5d2a08
Daniel_Ruiz <shintaruruiz@gmail.com>
2022-08-03 09:31:07 +0800

    added commands

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -2,3 +2,5 @@ alias edbash='nano /home/sekiro/.bashrc'
 alias edcomm='code /home/sekiro/.common_procedures/'
 alias edalias='nano /home/sekiro/.common_procedures/scripts/aliases.sh'
 alias john-repelant='gsettings set org.gnome.desktop.session idle-delay'
+
+alias diff_ws='code /home/sekiro/differentiate_teleop_ws/ && code /home/sekiro/differentiate_teleop_ws/src/pellion_robot_ws/ && cd /home/sekiro/differentiate_teleop_ws'
