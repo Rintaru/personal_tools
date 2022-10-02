4b57af4b6c3d134755f14c07e57e6945ee00bd7e
Daniel_Ruiz <shintaruruiz@gmail.com>
2022-10-02 11:27:19 +0800

    added commands

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -4,6 +4,7 @@ alias edalias='nano /home/sekiro/.common_procedures/scripts/aliases.sh'
 alias john-repelant='gsettings set org.gnome.desktop.session idle-delay'
 
 alias diff_ws='code /home/sekiro/differentiate_teleop_ws/ && code /home/sekiro/differentiate_teleop_ws/src/pellion_robot_ws/ && cd /home/sekiro/differentiate_teleop_ws'
+alias cd_assimov='cd /home/sekiro/differentiate_teleop_ws/src/pelion_robot_ws/src/assimov_gps/'
 alias hmi_ws='cd /home/sekiro/hmi_ws/human_machine_interface && code /home/sekiro/hmi_ws/human_machine_interface/ && code /home/sekiro/hmi_ws/'
 
 alias pub_bunker_rc='rostopic pub --once /bunker_rc sensor_msgs/Joy "header:
@@ -57,6 +58,22 @@ buttons: [0, 0, 0, 2]" && rostopic pub --once /bunker_rc sensor_msgs/Joy "header
 axes: [0.0, 0.0, 0.0, 0.0, 0.0]
 buttons: [0, 0, 0, 2]"'
 
+
+alias clr_mish='rosservice call /asv/mission/new_mission "input:
+  header:
+    seq: 0
+    stamp: {secs: 0, nsecs: 0}
+    frame_id: ''
+  mission_id: 0
+  uuid: ''
+  array: [ ]
+  start_time: {secs: 0, nsecs: 0}
+  elapsed_time: {secs: 0, nsecs: 0}
+  mission_eta: {secs: 0, nsecs: 0}
+  repeat_mission: false
+  reverse_on_repeat: false"'
+
+alias get_mish='rosservice call /asv/mission/get_current_mission "{}"'
 alias create_pkg='source /home/sekiro/chironix-create-pkg/scripts/chironix-create-pkg'
 
 
