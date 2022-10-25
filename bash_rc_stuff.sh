e770d69da64b817b85818428dbf1efd5e1c2b72c
Daniel_Ruiz <shintaruruiz@gmail.com>
2022-10-25 19:44:55 +0800

    fix: not dependent on user name anymore

--- bash_rc_stuff.sh
+++ bash_rc_stuff.sh
@@ -1,4 +1,4 @@
-source /home/sekiro/.common_procedures/scripts/common_procedures.sh
-source /home/sekiro/.common_procedures/scripts/aliases.sh
-source /home/sekiro/.common_procedures/scripts/make_catkin_worspace.sh
-source /home/sekiro/.common_procedures/scripts/capture_and_prev.sh
+source $HOME/.common_procedures/scripts/common_procedures.sh
+source $HOME/.common_procedures/scripts/aliases.sh
+source $HOME/.common_procedures/scripts/make_catkin_worspace.sh
+source $HOME/.common_procedures/scripts/capture_and_prev.sh
