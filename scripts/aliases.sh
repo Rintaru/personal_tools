a894b9ec719be90da5a31f8f57378ee4881b5549
Rintaru <shintaruruiz@gmail.com>
2024-03-06 21:16:20 +0800

    really old stuff

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -82,3 +82,31 @@ alias create_pkg='source $HOME/-REDACTED--create-pkg/scripts/-REDACTED--create-p
 alias vcan0_up='sudo ip link add dev vcan0 type vcan && sudo ip link set up vcan0 && candump vcan0'
 alias vcan0_down='sudo ip link set down vcan0 && sudo ip link delete vcan0'
 
+alias open='xdg-open'
+
+alias lemme_in_aht003='ssh rtkuser@192.168.121.21'
+
+
+
+
+
+
+alias docker_login='docker login artifactory.fmgl.com.au -u ams_ocs_docker_pullbot -p MwY7Gq0zT2XnovbT6zYD'
+
+alias fatos_pull='docker pull artifactory.fmgl.com.au/docker-ams-ocs-ocs/fortescue-autonomy/ocs:fatos-latest-master'
+
+alias fatos_enter='docker run -h fatos -u user -v /home/hensel/workspace:/home/user/workspace -w /home/user -i -t artifactory.fmgl.com.au/docker-ams-ocs-ocs/fortescue-autonomy/ocs:fatos-latest-master bash -l'
+
+alias rtkos_pull='docker pull artifactory.fmgl.com.au/docker-ams-ocs-rtk/fortescue-autonomy/rtk:rtk_os_dev_core-0.1.0-stable'
+
+alias rtkos_enter='docker run -h rtkos -u rtkuser -v /home/hensel/workspace:/home/rtkuser/workspace -w /home/rtkuser -i -t artifactory.fmgl.com.au/docker-ams-ocs-rtk/fortescue-autonomy/rtk:rtk_os_dev_core-0.1.0-stable bash -l'
+
+alias gimmie_branch='/home/sekiro/workspace/fat-prepare-git/fat_prepare_git.py'
+
+alias lemme_in_docker='docker exec -it --user=rtkuser --workdir="/home/rtkuser/workspace" vsc-rtk-os-dev-aht /bin/bash -l'
+
+
+alias test='echo "one"'
+alias test='echo "two"'
+alias mark_current_log_file='echo "kill me" >> /mnt/sysid_log_list.txt && cat /mnt/sysid_log_list.txt'
+alias mark_current_log_file='sudo echo "please kill me" >> /mnt/sysid_log_list.txt && cat /mnt/sysid_log_list.txt'
